f68f925e50da8b76582c5d21f13913e8701314fc
thib774 <thibault.petitjean@yahoo.fr>
2017-01-25 17:17:30 +0100

    Delete avancer_dst_sonar.h

--- BreakingBot/src/avancer_dst_sonar.h
+++
@@ -1,1 +0,0 @@
-void avancer_dst_sonar(uint8_t sn_right, uint8_t sn_left, uint8_t sn_clamp, uint8_t sn_gyro, uint8_t sn_sonar, uint8_t motor[2], float value, float gyro_val, float gyro_abs, FLAGS_T state, float dist);
