bed833f73112d92da933a177d44076dd310b154f
thib774 <thibault.petitjean@yahoo.fr>
2017-01-25 17:18:54 +0100

    Delete grab_gauche.h

--- BreakingBot/src/grab_gauche.h
+++
@@ -1,2 +0,0 @@
-void grab_gauche(uint8_t sn_right, uint8_t sn_left, uint8_t sn_clamp, uint8_t sn_gyro, uint8_t sn_sonar, uint8_t motor[2], float value, float gyro_val, float gyro_abs, FLAGS_T state);
-
